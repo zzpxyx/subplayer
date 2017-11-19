f61f9fb5066e098d1a6bbf7fde7c27ca40770038
zzpxyx <zptomz@gmail.com>
2017-11-18 21:00:09 -0600

    Add Event class.

---
+++ src/main/java/com/zzpxyx/subplayer/event/Event.java
@@ -0,0 +1,25 @@
+package com.zzpxyx.subplayer.event;
+
+public class Event implements Comparable<Event> {
+	public enum Type {
+		Start, End
+	}
+
+	public Type type;
+	public long time;
+	public String text;
+
+	public Event() {
+	}
+
+	public Event(Type type, long time, String text) {
+		this.type = type;
+		this.time = time;
+		this.text = text;
+	}
+
+	@Override
+	public int compareTo(Event anotherEvent) {
+		return Long.compare(this.time, anotherEvent.time);
+	}
+}
